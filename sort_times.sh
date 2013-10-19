a5e9578cea12b511655ee25fd4778e87bdc2f91b
Max Metcalfe <mmetcalfe@apple.com>
2013-10-18 18:33:28 -0700

    Added documentation to sort_times.sh

--- sort_times.sh
+++ sort_times.sh
@@ -1,18 +1,31 @@
+# This script compiles the times for the Fog City Run results
+# Max Metcalfe
 
+# Specify input/ouput files
 INPUT=/Users/maxmetcalfe/Documents/fog_city_run/Results/all.csv
 OUT=/Users/maxmetcalfe/Documents/fog_city_run/Results/compiled.txt
+
+# Get unique racer names
 UNIQUE_NAMES=`cut -d"," -f3,4 $INPUT | sort | uniq`
 
+# Print header to output file
 echo "Results" >> $OUT
 
+# Loop through unique racer names
 for i in $UNIQUE_NAMES
 do 
 
+	# Print racer name and format
 	echo $i >> $OUT
 	echo "-----" >> $OUT
 	
+	# Get results for racer, sort, and format
 	RESULTS=`grep $i $INPUT | sort -t":" -k2 | sed 's/ /\n/g' | cut -d"," -f-6,9 | tr ',' '\t    '`
 
+	# Write results to output
 	echo "$RESULTS" >> $OUT
+	# Add newline
 	echo "\n" >> $OUT
+
+# We are done
 done
