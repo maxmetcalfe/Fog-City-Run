764d84c00b82b7aab72e1f3e8a0704aed86978c1
Max Metcalfe <m.maxmetcalfe@gmail.com>
2013-10-20 22:32:29 -0700

    Fixed time sorting issue, for good

--- sort_times.sh
+++ sort_times.sh
@@ -2,8 +2,8 @@
 # Max Metcalfe
 
 # Specify input/ouput files
-INPUT=/Users/max/Documents/Home/Results/all.csv
-OUT=/Users/max/Desktop/compiled.txt
+INPUT=/Users/maxmetcalfe/Documents/fog_city_run/Results/all.csv
+OUT=/Users/maxmetcalfe/Documents/fog_city_run/Results/compiled.txt
 
 # Get unique racer names
 UNIQUE_NAMES=`cut -d"," -f3,4 $INPUT | sort | uniq`
@@ -20,7 +20,8 @@ do
 	echo "-----" >> $OUT
 	
 	# Get results for racer, sort, and format
-	RESULTS=`grep $i $INPUT | cut -d"," -f-6,9 | sort -k6 | sed 's/ /\n/g' | tr ',' '\t    '`
+	# RESULTS=`grep $i $INPUT | cut -d"," -f-6,9 | sort -k6 | sed 's/ /\n/g' | tr ',' '\t    '`
+	RESULTS=`grep $i $INPUT | cut -d"," -f-6,9 | sort -t , -k6`
 
 	# Write results to output
 	echo "$RESULTS" >> $OUT
