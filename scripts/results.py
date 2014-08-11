20a34c65925237c0c678b9acd0aeba4e149463b7
Max Metcalfe <mmetcalfe@apple.com>
2014-08-10 17:56:29 -0700

    Added automated upload for racer counts to results.py

--- scripts/results.py
+++ scripts/results.py
@@ -1,5 +1,6 @@
 import string
 import os
+import subprocess
 
 results_file = open("/Users/max/Downloads/racesplitter_race.csv", "r")
 os.system("grep -v '];' ../data/data.js > tmp.txt")
@@ -28,7 +29,7 @@ upload_file.write("];")
 results_file.close()
 upload_file.close()
 os.system("cat tmp.txt tmp2.txt > ../data/data.js")
-os.system("rm tmp.txt && rm tmp2.txt")
+os.system("rm tmp.txt tmp2.txt")
 os.system("sed -i '' -e's/\]$/\],/' ../data/data.js")
 os.system("git add ../data/data.js")
 os.system("git commit -m 'Added results from  " + race_date + "'")
@@ -36,5 +37,13 @@ os.system("git fetch origin")
 os.system("git rebase origin/master")
 os.system("git push origin master")
 os.system("rm /Users/max/Downloads/racesplitter_race.csv")
+race_data = race_date.replace("/", "\/")
+p = subprocess.Popen(["grep",race_date,"../data/data.js","-c"], stdout=subprocess.PIPE)
+count = p.communicate()[0][:-1]
+
+with open("../data/data.tsv", "a") as racer_count_file:
+	    racer_count_file.write(race_date + "\t" + count)
+
+os.system("ftp -in -u ftp://maxmetcalfe@maxmetcalfe.com/fcr/data/ ../data/data.tsv")
 
 print "Done."
