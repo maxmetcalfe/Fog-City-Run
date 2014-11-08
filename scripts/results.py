0aa33d9579c78f3d103bc0644ada81e5348c0e8a
Max Metcalfe <mmetcalfe@apple.com>
2014-11-08 07:58:02 -0800

    Adding top_racers.txt

--- scripts/results.py
+++ scripts/results.py
@@ -31,7 +31,7 @@ upload_file.close()
 os.system("cat tmp.txt tmp2.txt > ../data/data.js")
 os.system("rm tmp.txt")
 os.system("sed -i '' -e's/\]$/\],/' ../data/data.js")
-os.system("git add ../data/data.js")
+#os.system("git add ../data/data.js")
 os.system("rm /Users/max/Downloads/racesplitter_race.csv")
 race_data = race_date.replace("/", "\/")
 p = subprocess.Popen(["grep",race_date,"../data/data.js","-c"], stdout=subprocess.PIPE)
@@ -40,13 +40,13 @@ count = p.communicate()[0][:-1]
 with open("../data/data.tsv", "a") as racer_count_file:
 	    racer_count_file.write(race_date + "\t" + count)
 
-os.system("git add ../data/data.tsv")
-os.system("ftp -in -u ftp://maxmetcalfe@maxmetcalfe.com/fcr/data/ ../data/data.tsv")
-os.system("ftp -in -u ftp://maxmetcalfe@maxmetcalfe.com/fcr/data/ ../data/data.js")
-os.system("git commit -m 'Added results from  " + race_date + "'")
-os.system("git fetch origin")
-os.system("git rebase origin/master")
-os.system("git push origin master")
+#os.system("git add ../data/data.tsv")
+#os.system("ftp -in -u ftp://maxmetcalfe@maxmetcalfe.com/fcr/data/ ../data/data.tsv")
+#os.system("ftp -in -u ftp://maxmetcalfe@maxmetcalfe.com/fcr/data/ ../data/data.js")
+#os.system("git commit -m 'Added results from  " + race_date + "'")
+#os.system("git fetch origin")
+#os.system("git rebase origin/master")
+#os.system("git push origin master")
 # Test: Send tweet of winner. Should use Twitter API call.
 b = subprocess.Popen(["head","-n1","tmp2.txt"], stdout=subprocess.PIPE)
 winner = string.split(b.communicate()[0], ",")
@@ -56,9 +56,9 @@ time = winner[5][1:-1]
 msg = first_name + " " + last_name + " won the Fog City Run this week with a time of " + time + "."
 command = 'twitter -efogcityrun@email.com set %s' % msg
 os.system("rm tmp2.txt")
-subprocess.call(command, shell=True)
+#subprocess.call(command, shell=True)
 print "Tweet posted."
 print "Done."
 
 # Create top racers list (for future use in website table)
-os.system("grep "" ../data/data.js | awk -F"," ' { print $4 $3}' | sed "s/''/ /g" | sed "s/'//g" | sortit | grep "[0-9][0-9]" | sed 's/^ *//g' > top_racers.txt")
+os.system('''grep "" ../data/data.js | awk -F"," ' { print $4 $3}' | sed -e "s/''/ /g" -e "s/'//g" | sort | uniq -c | sort -nr | grep "[0-9][0-9]" > top_racers.txt''')
