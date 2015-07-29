da3c94050a6f2d7b3e46067c5670584844fb27b2
Max Metcalfe <m.maxmetcalfe@gmail.com>
2015-07-29 16:51:35 -0700

    Fixing FTP for new GoDaddy setup

--- scripts/results.py
+++ scripts/results.py
@@ -217,10 +217,10 @@ def git(race_date):
 	os.system("git fetch origin")
 	os.system("git rebase origin/master")
 	os.system("git push origin master")
-	os.system("ftp -in -u ftp://maxmetcalfe@maxmetcalfe.com/fcr/data/ ../data/racers.txt")
-	os.system("ftp -in -u ftp://maxmetcalfe@maxmetcalfe.com/fcr/data/ ../data/records.txt")
-	os.system("ftp -in -u ftp://maxmetcalfe@maxmetcalfe.com/fcr/data/ ../data/data.tsv")
-	os.system("ftp -in -u ftp://maxmetcalfe@maxmetcalfe.com/fcr/data/ ../data/data.js")
+	os.system("ftp -in -u ftp://newmaxmetcalfe@ftp.maxmetcalfe.com/public_html/www/fcr/data/ ../data/racers.txt")
+	os.system("ftp -in -u ftp://newmaxmetcalfe@ftp.maxmetcalfe.com/public_html/www/fcr/data/ ../data/records.txt")
+	os.system("ftp -in -u ftp://newmaxmetcalfe@ftp.maxmetcalfe.com/public_html/www/fcr/data/ ../data/data.tsv")
+	os.system("ftp -in -u ftp://newmaxmetcalfe@ftp.maxmetcalfe.com/public_html/www/fcr/data/ ../data/data.js")
 
 # Tweet winner
 def tweet_winner(racers):
